2f06e522d9d9e36ffec8fbbc2ba8ca48c7982d0a
Derek <stiau334@gmail.com>
2019-01-24 12:07:09 +0800

    Convert to MVC design pattern.

---
+++ Todolist/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todolist
+//
+//  Created by Derek on 2019/1/24.
+//  Copyright © 2019 Derek. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var done = false
+}
